279a07425e2a249c351203e5831fabc2c33588a7
bobur2006coder <boburdilmurodov702@gmail.com>
2023-03-30 13:55:01 +0500

    fix

--- javascript.js
+++ javascript.js
@@ -4,8 +4,9 @@ let b = document.querySelector("button");
 b.onclick =()=> {
     if(a.value==1234){
         alert("siz parolni to`g`ri kiritdingiz");
-        b.style.color="green";
+        b.style.color="white";
         b.style.marginTop="100px";
+        b.style.background="green"
     }else{
         alert("siz xato parol kiritdingiz")
     };
